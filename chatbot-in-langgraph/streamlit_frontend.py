fb86029a18172febf6660eaaad144ec43abb9b5e
Prath <prathme47@gmail.com>
2026-03-07 14:55:43 +0530

    basic implementation

--- chatbot-in-langgraph/streamlit_frontend.py
+++ chatbot-in-langgraph/streamlit_frontend.py
@@ -1,4 +1,8 @@
+from langchain_core.messages import HumanMessage
 import streamlit as st
+from langgraph_backend import chatbot
+
+config = {'configurable': {'thread_id': '1'}}
 
 if 'message_history' not in st.session_state:
     st.session_state.message_history = []
@@ -14,6 +18,9 @@ if user_input:
     with st.chat_message('user'):
         st.text(user_input)
 
-    st.session_state.message_history.append({'role': 'assistant', 'content': user_input})
+    response = chatbot.invoke({'messages': [HumanMessage(content=user_input)]}, config=config)
+    ai_message = response['messages'][-1].content
+
+    st.session_state.message_history.append({'role': 'assistant', 'content': ai_message})
     with st.chat_message('assistant'):
-        st.text(user_input)
+        st.text(ai_message)
