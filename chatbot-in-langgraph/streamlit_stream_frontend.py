deb5b07d870c8c38ff633d6a2c2f23428de71681
Prath <prathme47@gmail.com>
2026-03-09 23:30:03 +0530

    feat: Add two Streamlit frontends for the LangGraph chatbot, one supporting multi-conversation threading and another providing basic streaming.

---
+++ chatbot-in-langgraph/streamlit_stream_frontend.py
@@ -0,0 +1,33 @@
+from langchain_core.messages import HumanMessage
+import streamlit as st
+from langgraph_backend import chatbot
+
+config = {'configurable': {'thread_id': '1'}}
+
+if 'message_history' not in st.session_state:
+    st.session_state.message_history = []
+
+for message in st.session_state.message_history:
+    with st.chat_message(message['role']):
+        st.text(message['content'])
+
+user_input = st.chat_input("Input: ")
+
+if user_input:
+    st.session_state.message_history.append({'role': 'user', 'content': user_input})
+    with st.chat_message('user'):
+        st.text(user_input)
+
+    # st.session_state.message_history.append({'role': 'assistant', 'content': ai_message})
+    with st.chat_message('assistant'):
+        ai_message = st.write_stream(
+            chatbot.stream(
+                message_chunk.content for message_chunk, metadata in chatbot.stream(
+                    {'messages': [HumanMessage(content=user_input)]},
+                    config=config,
+                    stream_mode='messages'
+                )
+            )
+        )
+    
+    st.session_state.message_history.append({'role': 'assistant', 'content': ai_message})
